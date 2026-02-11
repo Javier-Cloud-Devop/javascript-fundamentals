09bfa072ffb167c01927711b8a2090afd2178692
Javier-Cloud-Devop <javiergonzruiz@gmail.com>
2026-02-11 07:03:42 +0000

    Create magic-eight-ball.js

---
+++ Conditional/magic-eight-ball.js
@@ -0,0 +1,41 @@
+let userName = 'Eddy';
+
+userName
+  ? console.log(`Hello, ${userName}!`)
+  : console.log('Hello!');
+
+let userQuestion = 'Will I have a third child?';
+
+console.log(`${userName} asks: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy, try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  default:
+    eightBall = 'Signs point to yes';
+}
+
+console.log(eightBall);
